0f3953ac193b1d997ad42d82d19d492194efcf12
84snel <84snel@gmail.com>
2020-12-14 02:44:37 +0200

    solved 4 from 7

---
+++ js/task-3.js
@@ -0,0 +1,41 @@
+const findBestEmployee = function (employees) {
+  const arrs = Object.entries(employees);
+
+  let maxNumberOfTask = 0;
+  let bestEmployee;
+
+  for (let arr of arrs) {
+    if (arr[1] > maxNumberOfTask) {
+      maxNumberOfTask = arr[1];
+      bestEmployee = arr[0];
+    }
+  }
+
+  return bestEmployee;
+};
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
